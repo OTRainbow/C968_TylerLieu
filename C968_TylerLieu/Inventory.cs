d2a57afba5f31af4dbb3b80040a15ca1e3a9552a
Tyler Lieu <tylerlieu0718@gmail.com>
2022-04-23 16:58:22 -0500

    Created Classes

---
+++ C968_TylerLieu/Inventory.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace C968_TylerLieu
+{
+    class Inventory
+    {
+        public BindingList<Product> Products { get; set; }
+        public BindingList<Part> AllParts { get; set; }
+        public void addProduct(Product product)
+        {
+
+        }
+        public bool removeProduct(int ProductID)
+        {
+            return true;
+        }
+        public Product lookupProduct(int ProductID)
+        {
+            return null;
+        }
+        public void updateProduct(int ProductID, Product product)
+        {
+
+        }
+        public void addPart(Part part)
+        {
+
+        }
+        public bool deletePart(Part part)
+        {
+            return true;
+        }
+        public Part lookupPart(int PartID)
+        {
+            return null;
+        }
+        public void updatePart(int PartID, Part part)
+        {
+
+        }
+    }
+}
---
+++ C968_TylerLieu/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace C968_TylerLieu
+{
+    class Product
+    {
+        public BindingList<Part> AssociatedParts { get; set; }
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int InStock { get; set; }
+        public int Min { get; set; }
+        public int Max { get; set; }
+        public void addAssociatedPart(Part part)
+        {
+
+        }
+        public bool removeAssociatedPart(int PartID)
+        {
+            return true;
+        }
+        public Part lookupAssociatedPart(int PartID)
+        {
+            return null;
+        }
+    }
+}
